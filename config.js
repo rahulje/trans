8901c1520ac77ad91613e51c50cb62402903fabb
rahulje <jerahul745@gmail.com>
2020-05-02 17:16:02 +0530

    ready for heroku

--- config.js
+++ config.js
@@ -1,14 +1,3 @@
-
-
-const mysql = require("mysql");
-const dbConfig = require("../config/db.config.js");
-
-var connection = mysql.createPool({
-  host: dbConfig.HOST,
-  user: dbConfig.USER,
-  password: dbConfig.PASSWORD,
-  database: dbConfig.DB
-});
-
+var connection = mysql.createConnection(process.env.DATABASE_URL)
 module.exports = connection;
 
